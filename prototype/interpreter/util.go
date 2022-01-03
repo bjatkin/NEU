e56b7389ded6e7c6852bb2c3401932da1339d34d
Brandon Atkinson <bj.atkinson92@gmail.com>
2022-01-03 13:05:09 -0700

    remove the old prototype

--- prototype/interpreter/util.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func bytesToInt(bytes []byte) int {
-	return int(bytes[0]) | int(bytes[1])<<8 | int(bytes[2])<<16 |
-		int(bytes[3])<<24 | int(bytes[4])<<32 | int(bytes[5])<<40 |
-		int(bytes[6])<<48 | int(bytes[7])<<56
-}
